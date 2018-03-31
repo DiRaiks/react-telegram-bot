2cb7bfbf90c3cdaef8a04981b0ef529822df7e16
DiRaiks <t.fane@mail.ru>
2018-03-31 21:32:11 +0300

    added bot

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import {Test} from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route path='/test' component={App}/>
+            <Route path='/' component={App}/>
+        </Switch>
+    </Router>, document.getElementById('root'));
 registerServiceWorker();
