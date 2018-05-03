88f45c1e04a5b7a9fd83c101569d363f3bf0598d
AndrewFinaev <a.finaev@dunice.net>
2018-05-03 11:47:55 +0300

    added webpack, babel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 // import {Test} from './routes';
 
 ReactDOM.render(
@@ -13,4 +12,3 @@ ReactDOM.render(
             <Route path='/' component={App}/>
         </Switch>
     </Router>, document.getElementById('root'));
-registerServiceWorker();
